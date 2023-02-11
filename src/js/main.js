4e7b7a1d61189410545677bb72b51dc9915c1fae
IngridRem <remenarova.ingrid@gmail.com>
2023-02-11 18:54:47 +0100

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-/*
-var button = document.querySelector ('#button')
-var modal = document.querySelector ('#modal')
-var close = document.querySelector ('#close')
-
-button.addEventListener ('click', function(){
-    modal.classList.add ('modal_active')
-})
-
-close.addEventListener ('click', function(){
-    modal.classList.remove ('modal_active')
-})
-*/
 
 $(document).ready (function(){
     var button = $('#button')
@@ -24,4 +11,26 @@ $(document).ready (function(){
     close.on('click', function(){
         modal.removeClass('modal_active')
     })
+
+    $('.slider').slick({
+        slidesToShow: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+            
+        ]    
+    
+    });
 })
